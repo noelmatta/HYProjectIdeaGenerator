5d48444878823314182e3ec06976a8281b8bee52
Noel Matta <noelmatta82@gmail.com>
2018-02-24 19:37:43 -0500

    rebuilt with new logic

--- dev/scripts/Content.js
+++ dev/scripts/Content.js
@@ -22,77 +22,59 @@ class Instructions extends React.Component {
 }
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      line    : 0,
+      nouns   : [],
+      verbs   : []
+    }
+  };
   render() {
     return (
       <section>
         <form action="">
-          <Verb />
-          <AdjNoun />
-          <AddLine />
-          <RemoveLine /><br />
-          <Conjunction />
+          <select>
+            {
+              this.state.verbs.map((verb) => {
+                return (
+                  <option key={verb}>{verb}</option>
+                )
+              })
+            }
+          </select>
+          <select>
+            {
+              this.state.verbs.map((verb) => {
+                return (
+                  <option key={verb}>{verb}</option>
+                )
+              })
+            }
+          </select>
         </form>
       </section>
     )
   }
-}
+  componentDidMount() {
+    const dbref = firebase.database().ref('/initialVerbs');
+    const initialVerbs = [];
+    dbref.on('value', (verbs) => {
+      const data = verbs.val();
 
-class AddLine extends React.Component {
-  render() {
-    return (
-      <button>+</button>
-    )
-  }
-}
-
-class RemoveLine extends React.Component {
-  render() {
-    return (
-      <button>-</button>
-    )
-  }
-}
-
-class Randomize extends React.Component {
-  render() {
-    return (
-      <button>randomize</button>
-    )
+      for (let key in data) {
+        data.key = key;
+        initialVerbs.push(data.key);
+      }
+      this.setState({
+        verbs: initialVerbs
+      });
+    });
   }
 }
 
-class AdjNoun extends React.Component {
-  render() {
-    return (
-      <select name="" id="">
-        <option value="adjNoun">Adjective + Noun</option>
-      </select>
-    )
-  }
-}
+class Line extends React.Component {
 
-class Verb extends React.Component {
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
-    console.log(e.target.value);
-  }
-  render() {
-    return (
-      <select name="" id="" value="" onChange={this.handleChange}>
-        <option value="on">Select A Verb</option>
-        <option value="test">Generates</option>
-        <option value="test2">Transforms</option>
-        <option value="test3">Calculates</option>
-        <option value="test4">Displays</option>
-        <option value="test5">Finds</option>
-        <option value="test6">Uses</option>
-        <option value="test7">Get A Random Verb</option>
-      </select>
-    )
-  }
 }
 
 
@@ -101,22 +83,6 @@ class Audience extends React.Component {
     return (
       <input type="checkbox" name="" id="" />
     )
-  }
-}
-
-class Conjunction extends React.Component {
-  render() {
-    return (
-      <select name="" id="">
-        <option value="and">and</option>
-        <option value="or">or</option>
-        <option value="from">from</option>
-        <option value="while">while</option>
-        <option value="into">into</option>
-        <option value="to">to</option>
-        <option value="for">for</option>
-        <option value="if">if</option>
-      </select>    )
   }
 }
 
