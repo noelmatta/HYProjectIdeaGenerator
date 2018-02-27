67c7961d136f0e88118809c064dc9a2be37cf77a
Noel Matta <noelmatta82@gmail.com>
2018-02-26 22:54:06 -0500

    functionality FINISHED

--- dev/scripts/Line.js
+++ dev/scripts/Line.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const Line = (props) => {
-    console.log(props.speechType)
     return (
-      <select onChange={props.buildIdea} label={props.speechType}>
+        <select onChange={props.buildIdea} data-name={props.speechType} label={props.speechType}>
       {
         props.data.map((key) => {
           return (
-            <option value={key} id={key} key={key} name='test'>{key}</option>
+            <option value={key} id={key} key={key} label={props.speechType} name='test'>{key}</option>
           )
         })
       }
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -21,24 +21,36 @@ class App extends React.Component {
   }
   render() {
     return(
-      <main>
-        <form>
-          <h1>Build A Web App That Will...</h1>
-          <Line data={this.state["verb-transitive"]} buildIdea={this.buildIdea} speechType="verb-transitive" />
-          <Line data={this.state["adjective"]} buildIdea={this.buildIdea} speechType="adjective" />
-          <Line data={this.state["noun"]} buildIdea={this.buildIdea} speechType="noun" />        
-        </form>
-        <span>
-          <h2>The Greatest Project Idea:</h2>
-          <h3>Build A Web App That Will {this.state.selectedVerb} {this.state.selectedAdjective} {this.state.selectedNoun}</h3>
-        </span>
-      </main>
+      <div className="wrapper">
+        <header>
+        </header>
+        <main>
+          <form>
+            <h1>The Unofficial HackerYou Project Idea Generator</h1>
+            <p>Select a verb, adjective, and noun:</p>
+            <Line data={this.state["verb-transitive"]} buildIdea={this.buildIdea} speechType="verb-transitive" />
+            <Line data={this.state["adjective"]} buildIdea={this.buildIdea} speechType="adjective" />
+            <Line data={this.state["noun"]} buildIdea={this.buildIdea} speechType="noun" />     
+          </form>
+          <span className="idea">
+            <h2>The Greatest Project Idea:</h2>
+            <h3>Build a web app that will {this.state.selectedVerb} {this.state.selectedAdjective} {this.state.selectedNoun}!!!</h3>
+          </span>
+        </main>
+      </div>
     )
   }
   buildIdea(fragments) {
-    this.setState({
-      selectedVerb: fragments.target.value
-    })
+    const target = fragments.target;
+    const speechType = target.dataset.name;
+
+    speechType === 'verb-transitive' ? this.setState({ selectedVerb: fragments.target.value })
+    :
+    speechType === 'adjective' ? this.setState({ selectedAdjective: fragments.target.value })
+    :
+    speechType === 'noun' ? this.setState({ selectedNoun: fragments.target.value }) : console.log('hey') ;
+    
+      
   }
   getWords(typeOfWord) {
     axios.get(`http://api.wordnik.com:80/v4/words.json/randomWords?&includePartOfSpeech=${typeOfWord}&limit=15&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`)
